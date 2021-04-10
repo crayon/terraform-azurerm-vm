122e6e9eb792107b226ad4de922b41036ba0353a
Roberth Strand <me@robstr.dev>
2021-04-10 12:31:11 +0200

    Removed unused locals

--- locals.tf
+++ locals.tf
@@ -1,6 +1,4 @@
 locals {
-  windows_vm      = var.source_image_reference.offer == "WindowsServer" ? 1 : 0
-  linux_vm        = var.source_image_reference.offer != "WindowsServer" ? 1 : 0
   linux_admin_ssh = lookup(var.admin_user, "ssh_key", null) != null ? true : false
 
   network_interface_ids = var.network_interface_ids != null ? var.network_interface_ids : [for ni in azurerm_network_interface.machine : ni.id]
