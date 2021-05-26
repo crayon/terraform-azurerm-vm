c27ccfe43304cd8a5350e25bfc7853519c01f4c6
Roberth Strand <me@robstr.dev>
2021-05-26 16:20:27 +0200

    Bug fix

--- locals.tf
+++ locals.tf
@@ -3,6 +3,8 @@ locals {
 
   network_interface_ids = var.network_interface_ids != null ? var.network_interface_ids : [for ni in azurerm_network_interface.machine : ni.id]
 
+  vm_id = local.os_type != "windows" ? azurerm_linux_virtual_machine.machine[0].id : azurerm_windows_virtual_machine.machine[0].id
+
   image_id_os = var.source_image_id.os == "windows" ? "windows" : "linux"
   image_reference_os = var.source_image_reference.offer == "WindowsServer" ? "windows" : "linux"
   os_type = var.source_image_id.os == null ? local.image_reference_os : local.image_id_os
