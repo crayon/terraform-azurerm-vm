8b1a6c33003b59fafaeb36e9f598891c0bbed178
Roberth Strand <me@robstr.dev>
2021-09-08 12:15:12 +0200

    fix #17, fix #18: Tested and validated.

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.54.0"
+      version = "2.75.0"
     }
   }
-  required_version = ">= 0.14.9"
+  required_version = ">= 1.0.6"
 }
