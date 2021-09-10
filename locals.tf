1e33e2df2feb48563cec0f65dbb931f3703f39b6
Roberth Strand <me@robstr.dev>
2021-09-10 09:32:44 +0200

    Updated image_reference_os, closes #23

--- locals.tf
+++ locals.tf
@@ -3,9 +3,8 @@ locals {
 
   network_interface_ids = var.network_interface_ids != null ? var.network_interface_ids : [for ni in azurerm_network_interface.machine : ni.id]
 
-  vm_id = local.os_type != "windows" ? azurerm_linux_virtual_machine.machine[0].id : azurerm_windows_virtual_machine.machine[0].id
-
+  vm_id              = local.os_type != "windows" ? azurerm_linux_virtual_machine.machine[0].id : azurerm_windows_virtual_machine.machine[0].id
   image_id_os        = var.source_image_id.os == "windows" ? "windows" : "linux"
-  image_reference_os = var.source_image_reference.offer == "WindowsServer" ? "windows" : "linux"
+  image_reference_os = can(regex("(Windows)", var.source_image_reference.offer)) ? "windows" : "linux"
   os_type            = var.source_image_id.os == null ? local.image_reference_os : local.image_id_os
 }
