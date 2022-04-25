e9e1144d83f079f901c354143a62f7885a7aa607
Roberth Strand <me@robstr.dev>
2022-04-25 10:45:16 +0200

    fix(availability-zones): set input variable to string
    Signed-off-by: Roberth Strand <me@robstr.dev>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.99.0"
+      version = ">= 3.0.0"
     }
   }
   required_version = ">= 1.0.6"
