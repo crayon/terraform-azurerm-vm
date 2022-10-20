a61ad29fb2007eea6ff1ab41e2e04dc7625a2345
molldk <klaus.mollnitz@crayon.com>
2022-10-20 22:34:52 +0200

    feat(outputs): added primary private ip as output
    Added primary private IP as output.
    
    Signed-off-by: molldk <klaus.mollnitz@crayon.com>

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "os_type" {
   description = "The OS used on this virtual machine."
   value       = title(local.os_type)
 }
+output "private_ip_address" {
+  description = "The primary private IP addresses assigned to the virtual machine."
+  value       = local.os_type != "windows" ? azurerm_linux_virtual_machine.machine[0].private_ip_address : azurerm_windows_virtual_machine.machine[0].private_ip_address
+}
