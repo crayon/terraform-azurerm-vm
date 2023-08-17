4bb500185ee1e05d7e2a5186f79417ee2ff6a721
Kim Andrè Nyhagen <kim.nyhagen@crayon.com>
2023-08-17 15:19:46 +0200

    Update main.tf
    Signed-off-by: Kim Andrè Nyhagen <kim@nyhagen.net>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.69.0"
+      version = ">= 3.64.0"
     }
   }
   required_version = ">= 1.5.0"
