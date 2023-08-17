9fd5ab864f6a15b147d61f3415d519cbba075ae1
Kim Andrè Nyhagen <kim.nyhagen@crayon.com>
2023-08-17 15:46:56 +0200

    Update vm_windows.tf
    Signed-off-by: Kim Andrè Nyhagen <kim@nyhagen.net>

--- vm_windows.tf
+++ vm_windows.tf
@@ -12,7 +12,7 @@ resource "azurerm_windows_virtual_machine" "machine" {
   network_interface_ids = local.network_interface_ids
   patch_mode            = var.patch_mode
   patch_assessment_mode = var.patch_mode
-  bypass_platform_safety_checks_on_user_schedule_enabled = var.bypass_platform_safety_checks
+  bypass_platform_safety_checks_on_user_schedule_enabled = var.bypass_platform_safety_checks_on_user_schedule_enabled
   os_disk {
     caching                   = var.os_disk.caching
     storage_account_type      = var.os_disk.storage_account_type
