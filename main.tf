3f5d1dcb1c026da1e62175472373a14525932b72
Kim Andrè Nyhagen <kim.nyhagen@crayon.com>
2023-08-17 15:21:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.64.0"
+      version = ">= 3.67.0"
     }
   }
   required_version = ">= 1.5.0"
